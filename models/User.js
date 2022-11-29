a795b21ddf66ffe3c97e23f9c7aa2e192bdfaacf
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 15:13:47 -0800

    add email to model

--- models/User.js
+++ models/User.js
@@ -9,6 +9,13 @@ const userSchema = new Schema(
             required: true,
             trim: true,
         },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [ isEmail, 'invalid email' ]
+        },
+        
     }
 );
 
