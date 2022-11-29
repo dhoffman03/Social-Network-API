49a5c84a1f9300bbf282d2fe87c774b47acc2fd6
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 15:28:26 -0800

    outling schemas

--- models/Thought.js
+++ models/Thought.js
@@ -2,17 +2,50 @@ const { Schema, model } = require('mongoose');
 
 const reactionSchema = new Schema(
     {
+        reactionId: {
 
+        },
+        reactionBody: {
+
+        },
+        username: {
+
+        },
+        createdAt: {
+            
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
 );
 
+// Schema to create Thought model
 const thoughtSchema = new Schema(
     {
+        thoughtText: {
 
+        },
+        createdAt: {
+
+        },
+        username: {
+
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
 );
 
-
+// Initialize Thought model
 const Thought = model('thought', thoughtSchema);
 
 module.exports = Thought;
