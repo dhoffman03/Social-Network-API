e2a9a798e3dd87274585d842a0027692d07513d5
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 18:33:23 -0800

    building server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+// Import dependencies
+const express = require('express');
+const mongoose = require('mongoose')
+
+const routes = require('./routes')
+
+const PORT = process.env.PORT || 3001;
+const app = express();
