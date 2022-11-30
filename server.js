7052141de670ddb081472060257b539724bd33f4
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 18:35:49 -0800

    added middleware

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ const routes = require('./routes')
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
