4f35c68458742994c06129ed6ea8bb7042e9ae6d
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 21:04:38 -0800

    getThoughtById added

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -5,8 +5,8 @@ module.exports = {
     // Get all thoughts
     getThoughts(req, res) {
         Thought.find({})
-        .then(thoughtData => res.json(thoughtData))
-        .catch((err) => res.status(500).json(err));
+            .then(thoughtData => res.json(thoughtData))
+            .catch((err) => res.status(500).json(err));
     },
     // Create a new thought
     createThought(req, res) {
@@ -15,7 +15,11 @@ module.exports = {
     // /api/users/:thoughtId
     // Get a single thought
     getThoughtById(req, res) {
-
+        Thought.findOne({ _id: req.params.thoughtId })
+            .then((thoughtData) =>
+                !thoughtData ? res.status(404).json({ message: 'No thought with that ID!' }) : res.json(thoughtData)
+            )
+            .catch((err) => res.status(500).json(err));
     },
     // Update a user's thought
     updateThought(req, res) {
