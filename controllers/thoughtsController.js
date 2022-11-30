4e826a09f093d4ddfb299655109820aa0f19fe87
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 21:00:00 -0800

    getThoughts added

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -3,10 +3,26 @@ const { Thought, User } = require('../models');
 module.exports = {
     // /api/users
     // Get all thoughts
+    getThoughts(req, res) {
+        Thought.find({})
+        .then(thoughtData => res.json(thoughtData))
+        .catch((err) => res.status(500).json(err));
+    },
     // Create a new thought
+    createThought(req, res) {
 
+    },
     // /api/users/:thoughtId
     // Get a single thought
+    getThoughtById(req, res) {
+
+    },
     // Update a user's thought
+    updateThought(req, res) {
+
+    },
     // Delete a user's thought
+    deleteThought(req, res) {
+
+    },
 }
