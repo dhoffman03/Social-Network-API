bb6d8405cd7a8d75f80c637446c884d404550b7f
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 20:37:00 -0800

    getUserById and createUser added

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -7,15 +7,29 @@ module.exports = {
         User.find({})
             .then(userData => res.json(userData))
             .catch((err) => res.status(500).json(err));
-    }
-
-    
-
-    // createUser
-
-
+    },
+    // POST route to create new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then(userData => res.json(userData))
+            .catch((err) => res.status(500).json(err));
+    },
     // /api/:userId
-    // getUserById
-    // updateUser
-    // deleteUser
+    // GET a single user
+    getUserById(req, res) {
+        User.findOne({ _id: req.params.userId })
+            .select('-__v')
+            .then((userData) =>
+                !userData ? res.status(404).json({ message: 'No user with that ID!' }) : res.json(userData)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // PUT route to update current user
+    updateUser(req, res) {
+        
+    },
+    // Delete a user
+    deleteUser(req, res) {
+
+    },
 }
