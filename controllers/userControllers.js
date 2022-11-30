cda0c0f5be7a9404bd8a1e9ad040deafcb3b6a35
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 20:16:14 -0800

    building controllers

---
+++ controllers/userControllers.js
@@ -0,0 +1,13 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // /api/users
+    // getAllUsers
+    // createUser
+
+
+    // /api/:userId
+    // getUserById
+    // updateUser
+    // deleteUser
+}
