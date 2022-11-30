3cf85775d69485e0984f3b6eb4a3e5a0fb116f92
Dymond <dymond.hoffman03@gmail.com>
2022-11-29 19:58:34 -0800

    pseudocode

---
+++ routes/api/thoughtsRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+// getAllThoughts - GET
+// getThoughtById - GET by _id
+// createThought - POST & push _id to associated user's thoughts array
+// updateThought - PUT by _id
+// deleteThought - DELETE by _id
---
+++ routes/api/usersRoutes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+// getAllUsers - GET
+// getUserById - GET by _id
+// createUser - POST
+// updateUser - PUT by _id
+// deleteUser - DELETE by _id
